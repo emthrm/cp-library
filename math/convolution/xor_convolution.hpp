f7506537226bd8b60c1034381502a0797f17b17e
emthrm <38392306+emthrm@users.noreply.github.com>
2021-09-20 02:24:30 +0900

    Add a reference of fast Walsh-Hadamard transform

--- math/convolution/xor_convolution.hpp
+++ math/convolution/xor_convolution.hpp
@@ -10,22 +10,30 @@
 template <typename T>
 std::vector<T> xor_convolution(std::vector<T> a, std::vector<T> b, const T ID = 0) {
   int n = std::max(a.size(), b.size()), p = 1;
-  while ((1 << p) < n) ++p;
+  while ((1 << p) < n) {
+    ++p;
+  }
   n = 1 << p;
   a.resize(n, ID);
   b.resize(n, ID);
   auto fwht = [n](std::vector<T> &v) -> void {
-    for (int i = 1; i < n; i <<= 1) for (int s = 0; s < n; ++s) {
-      if (s & i) continue;
-      T tmp1 = v[s], tmp2 = v[s | i];
-      v[s] = tmp1 + tmp2;
-      v[s | i] = tmp1 - tmp2;
+    for (int i = 1; i < n; i <<= 1) {
+      for (int s = 0; s < n; ++s) {
+        if (s & i) continue;
+        T tmp1 = v[s], tmp2 = v[s | i];
+        v[s] = tmp1 + tmp2;
+        v[s | i] = tmp1 - tmp2;
+      }
     }
   };
   fwht(a);
   fwht(b);
-  for (int i = 0; i < n; ++i) a[i] *= b[i];
+  for (int i = 0; i < n; ++i) {
+    a[i] *= b[i];
+  }
   fwht(a);
-  for (int i = 0; i < n; ++i) a[i] /= n;
+  for (int i = 0; i < n; ++i) {
+    a[i] /= n;
+  }
   return a;
 }
