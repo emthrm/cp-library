01fcd60f15d4ded38f5242d203580183c23b29fc
emthrm <38392306+emthrm@users.noreply.github.com>
2021-12-05 03:50:04 +0900

    Change the position of `&` in my template

--- util/template.hpp
+++ util/template.hpp
@@ -16,8 +16,8 @@ constexpr int MOD = 1000000007;
 // constexpr int MOD = 998244353;
 constexpr int DY[]{1, 0, -1, 0}, DX[]{0, -1, 0, 1};
 constexpr int DY8[]{1, 1, 0, -1, -1, -1, 0, 1}, DX8[]{0, -1, -1, -1, 0, 1, 1, 1};
-template <typename T, typename U> inline bool chmax(T &a, U b) { return a < b ? (a = b, true) : false; }
-template <typename T, typename U> inline bool chmin(T &a, U b) { return a > b ? (a = b, true) : false; }
+template <typename T, typename U> inline bool chmax(T& a, U b) { return a < b ? (a = b, true) : false; }
+template <typename T, typename U> inline bool chmin(T& a, U b) { return a > b ? (a = b, true) : false; }
 struct IOSetup {
   IOSetup() {
     std::cin.tie(nullptr);
