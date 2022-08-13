ce861d85b1e43511abe73d5e9a47128d8e73114d
emthrm <38392306+emthrm@users.noreply.github.com>
2022-08-13 17:26:49 +0900

    Change the test of Pascal's triangle

--- test/math/twelvefold_way/pascal.test.cpp
+++ test/math/twelvefold_way/pascal.test.cpp
@@ -1,31 +1,21 @@
 /*
  * @brief 数学/写像12相/パスカルの三角形
  */
-#define PROBLEM "https://yukicoder.me/problems/no/420"
+#define PROBLEM "https://atcoder.jp/contests/abc254/tasks/abc254_b"
 
 #include <iostream>
 #include <vector>
 
-#include "../../../math/bigint.hpp"
 #include "../../../math/twelvefold_way/pascal.hpp"
 
 int main() {
-  using bigint = BigInt<>;
-  constexpr int B = 31;
-  const std::vector<std::vector<bigint>> c = pascal<bigint>(B);
-  int x;
-  std::cin >> x;
-  if (x == 0) {
-    std::cout << 1 << ' ' << 0 << '\n';
-  } else if (x > B) {
-    std::cout << 0 << ' ' << 0 << '\n';
-  } else {
-    std::cout << c[B][x] << ' ';
-    bigint ans = 0;
-    for (int b = 0; b < B; ++b) {
-      ans += c[B - 1][x - 1] * (1 << b);
+  int n;
+  std::cin >> n;
+  const std::vector<std::vector<int>> ans = pascal<int>(n - 1);
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j <= i; ++j) {
+      std::cout << ans[i][j] << " \n"[j == i];
     }
-    std::cout << ans << '\n';
   }
   return 0;
 }
