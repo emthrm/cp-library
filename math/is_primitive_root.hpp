b9a98585456226fdddc82ff885afe0e872ce306c
emthrm <38392306+emthrm@users.noreply.github.com>
2022-10-30 00:58:57 +0900

    Fix `is_primitive_root`

--- math/is_primitive_root.hpp
+++ math/is_primitive_root.hpp
@@ -13,23 +13,21 @@
 #include "./mod_pow.hpp"
 #include "./prime_factorization.hpp"
 
-bool is_primitive_root(long long root, const long long m) {
+bool is_primitive_root(long long root, const int m) {
   if ((root %= m) < 0) root += m;
-  if (std::__gcd(root, m) > 1) return false;
-  static std::map<long long, long long> phi;
+  if (std::__gcd(static_cast<int>(root), m) > 1) return false;
+  static std::map<int, int> phi;
   if (!phi.count(m)) phi[m] = euler_phi(m);
-  const long long phi_m = phi[m];
-  static std::map<long long, std::vector<long long>> primes;
+  const int phi_m = phi[m];
+  static std::map<int, std::vector<int>> primes;
   if (!primes.count(phi_m)) {
-    const std::vector<std::pair<long long, int>> prime_factors =
-        prime_factorization(phi_m);
-    std::vector<long long> tmp;
-    for (const std::pair<long long, int>& pr : prime_factors) {
+    std::vector<int> tmp;
+    for (const std::pair<int, int>& pr : prime_factorization(phi_m)) {
       tmp.emplace_back(pr.first);
     }
     primes[phi_m] = tmp;
   }
-  for (const long long p : primes[phi_m]) {
+  for (const int p : primes[phi_m]) {
     if (mod_pow(root, phi_m / p, m) == 1) return false;
   }
   return true;
