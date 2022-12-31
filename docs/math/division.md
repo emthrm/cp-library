6a3a07c252d62b0361863f9989bff71886ab82e7
emthrm <38392306+emthrm@users.noreply.github.com>
2022-12-31 20:40:34 +0900

    Add floor/ceil division

---
+++ docs/math/division.md
@@ -0,0 +1,18 @@
+---
+title: 切り捨て除算 (floor division)・切り上げ除算
+documentation_of: include/emthrm/math/division.hpp
+---
+
+
+## 使用法
+
+||説明|
+|:--:|:--:|
+|`floor_div(a, b)`|$\left\lfloor \frac{A}{B} \right\rfloor$|
+|`ceil_div(a, b)`|$\left\lceil \frac{A}{B} \right\rceil$|
+
+
+## 参考
+
+- https://twitter.com/noshi91/status/1439230342850695170
+- https://github.com/NyaanNyaan/library/blob/6abcedf4fd779ce0a42a8b50d3cbbfa3ae5aef1a/misc/int_div.hpp
---
+++ include/emthrm/math/division.hpp
@@ -0,0 +1,23 @@
+#ifndef EMTHRM_MATH_DIVISION_HPP_
+#define EMTHRM_MATH_DIVISION_HPP_
+
+#include <cassert>
+#include <type_traits>
+
+namespace emthrm {
+
+template <typename T, typename U>
+std::common_type_t<T, U> floor_div(const T a, const U b)  {
+  assert(b != 0);
+  return a / b - ((int{a < 0} ^ int{b < 0}) & int{a % b != 0});
+}
+
+template <typename T, typename U>
+std::common_type_t<T, U> ceil_div(const T a, const U b)  {
+  assert(b != 0);
+  return a / b + int{(a < 0) == (b < 0) && a % b != 0};
+}
+
+}  // namespace emthrm
+
+#endif  // EMTHRM_MATH_DIVISION_HPP_
