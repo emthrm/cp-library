10bb86d58731f64edc996b1d28704f9acd2b42b6
emthrm <38392306+emthrm@users.noreply.github.com>
2023-02-07 14:15:23 +0900

    Remove xorshift

--- include/emthrm/util/timer.hpp
+++ include/emthrm/util/timer.hpp
@@ -7,16 +7,19 @@ namespace emthrm {
 
 struct Timer {
   Timer() { reset(); }
+
   void reset() { bgn = std::chrono::high_resolution_clock::now(); }
+
   template <typename PeriodType = std::chrono::milliseconds>
   long long elapsed() const {
-    std::chrono::high_resolution_clock::time_point end =
+    const std::chrono::high_resolution_clock::time_point end =
         std::chrono::high_resolution_clock::now();
     return std::chrono::duration_cast<PeriodType>(end - bgn).count();
   }
+
  private:
   std::chrono::high_resolution_clock::time_point bgn;
-} timer;
+};
 
 }  // namespace emthrm
 
